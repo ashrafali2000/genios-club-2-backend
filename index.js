4c4a29cd11fcfa03489f0f53259a1fef8e460fe3
Zubair <zbrlohar@gmail.com>
2024-08-13 15:22:04 +0500

    test

--- index.js
+++ index.js
@@ -31,37 +31,38 @@ mongoose
     console.log("err------>", err);
   });
 // connect to your contract
-// const myContract = getContract({
-//   client,
-//   chain: defineChain(4002),
-//   address: process.env.CONTRACT_ADDRESS,
-// });
+const myContract = getContract({
+  client,
+  chain: defineChain(4002),
+  address: process.env.CONTRACT_ADDRESS,
+});
 
-// const preparedEvent = prepareEvent({
-//   myContract,
-//   signature:
-//     "event NewUser(address _user, address _sponcer, uint256 amount, uint256 time)",
-// });
+const preparedEvent = prepareEvent({
+  myContract,
+  signature:
+    "event NewUser(uint _user, uint _sponcer, uint256 amount, uint256 time)",
+});
 
-// const events = await getContractEvents({
-//   contract: myContract,
-//   fromBlock: 26562743n,
-//   toBlock: 26562797n,
-//   events: [preparedEvent],
-// });
 
-// const response = {
-//   events: events[0],
-// };
+const events = await getContractEvents({
+  contract: myContract,
+  fromBlock: 26562743n,
+  toBlock: 26562797n,
+  events: [preparedEvent],
+});
+
+const response = {
+  events: events[0],
+};
 
 // Convert BigInt values to strings for serialization
-// let serializedResponse;
-// setTimeout(() => {
-//   serializedResponse = JSON.stringify(response?.events?.args, (key, value) =>
-//     typeof value === "bigint" ? value.toString() : value
-//   );
-//   console.log(`These are all the events that we need:=> ${serializedResponse}`);
-// }, 2000);
+let serializedResponse;
+setTimeout(() => {
+  serializedResponse = JSON.stringify(response?.events?.args, (key, value) =>
+    typeof value === "bigint" ? value.toString() : value
+  );
+  console.log(`These are all the events that we need:=> ${serializedResponse}`);
+}, 2000);
 
 app.get("/", function (req, res) {
   res.send(`Server is running.....and...Data.....${serializedResponse}`);
